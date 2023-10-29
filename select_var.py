af251b74530581e3a512d4f34f7d0429d107e922
pabloChantada <147641118+pabloChantada@users.noreply.github.com>
2023-10-29 22:19:20 +0100

    Update select_var.py

--- select_var.py
+++ select_var.py
@@ -1,28 +1,28 @@
 import file_reader
 
 def show_columns(data, var = None):
-    titulo = data.columns # lista con la cabezera del dataframe
-    # imprimimos el indice y la columna
+    titulo = data.columns  # Lista con la cabezera del dataframe
+    # Imprimimos el indice y la columna
     for i in range(len(titulo)):
         print(f"{i}. {titulo[i]}")
-    # anadimos esta condicion en caso de que solo queramos imprimnir las columnas
+    # Anadimos esta condicion en caso de que solo queramos imprimnir las columnas
     if var is not None:
         # SE PUEDE REPETIR COLUMNA? PREGUNTAR
         while True:
             try:
                 var_selection = int(input(f'\nSeleccione el nombre de la columna para la variable {var}: '))
-                selection = data[titulo[var_selection]] # cojemos del df la columna de la cabezera(titulo) seleccionada
+                selection = data[titulo[var_selection]]  # Cojemos del df la columna de la cabezera(titulo) seleccionada
                 return selection
             except (IndexError, ValueError):
                 print('\nSeleccione un numero valido')
                 
-# abirmos el archivo y lo almacenamos en data
+# Abirmos el archivo y lo almacenamos en data
 data = file_reader.open_file()
-print(data) # mostramos el df al usuario
+print(data)  # Mostramos el df al usuario
 
 print('\nNOMBRE DE LAS COLUMNAS:')
-x = show_columns(data, 'x') # SELECCION DE VARIABLE X
-y = show_columns(data, 'y') # SELECCION DE VARIABLE Y
-# mostramos las variables
+x = show_columns(data, 'x')  # SELECCION DE VARIABLE X
+y = show_columns(data, 'y')  # SELECCION DE VARIABLE Y
+# Mostramos las variables
 print('\nLa variable x seleccionada es: ', x)
 print('\nLa variable y seleccionada es: ', y)
