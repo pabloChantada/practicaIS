d63145f764e1e0c2443ff0acc2365b4d38786ee1
ClaudiaVidalOtero <147641291+ClaudiaVidalOtero@users.noreply.github.com>
2023-11-15 00:00:45 +0100

    MRL testeo nueva
    Versión ya modificada de la MRL que funciona guardando las predicciones.

--- MRL_testeo.py
+++ MRL_testeo.py
@@ -1,26 +1,38 @@
-import pandas as pd
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 from scipy.stats import pearsonr
+import numpy as np
+from tkinter.messagebox import showinfo
+from predicciones_MRL import *
 
-# Crea un modelo de regresión lineal
-model = LinearRegression()
-model.fit(X, Y)
+def mrl_testeo(x, y, x_title, y_title):
+    '''
+    Muestra la grafica de dispersion, la regresion lineal, la correlacion y la prediccion de y.
+    '''                          
+    model = LinearRegression().fit(x, y)                                # Creamos el modelo de regresion lineal 
+    y_pred = model.predict(x)                                           # Predecimos los valores de y
+    x1 = np.reshape(x, -1)                                              # Redimensionamos los valores de x e y para  
+    y1 = np.reshape(y, -1)                                              # poder calcular la correlacion, con los vectores                                           
+    correlation, _ = pearsonr(x1, y1)                                   # normales (1D) no funciona
+    bondad = model.score(x, y)                                          # Calculamos la bondad de ajuste
+    Prediction = Predictions(x, y, x_title, y_title, correlation)       #almacenamos las variables y sus correlaciones en una clase
+    guardar_prediccion(Prediction, 'predicciones.pkl')                  #guardamos las predicciones en un archivo
+    reult_str = f"Bondad de ajuste: {bondad}\
+        \nCorrelación entre {x_title} e {y_title}: {correlation}"
 
-# Realizamos las predicciones
-Y_pred = model.predict(X)
+    plt.scatter(x, y)                                                   # Mostramos la grafica de dispersion
+    plt.plot(x, y_pred, color='red', 
+                                linewidth=2, 
+                                label='Regresión lineal')               # Mostramos la regresion lineal
+    plt.xlabel('VARIABLE X')                                            # Mostramos los titulo del eje x
+    plt.ylabel('VARIABLE Y')                                            # Mostramos los titulo del eje y
+    plt.legend()                                                        # Mostramos la leyenda
+    plt.title('Regresión Lineal y Correlación entre X e Y')             # Titulo de la grafica
+    plt.show()                                                          # Mostramos la grafica
+    showinfo("Resultados", reult_str)   # Mostramos los resultados en una ventana de mensaje
 
-# Calcular la correlación entre X e Y
-correlation, _ = pearsonr(data[columna_x], data[columna_y])
+predicciones_cargadas = cargar_predicciones('predicciones.pkl')
 
-# Imprime la correlación entre X e Y.
-print(f"Correlación entre {columna_x} y {columna_y}: {correlation:.2f}")
-
-# Visualización de los resultados en una gráficoa.
-plt.scatter(data[columna_x], data[columna_y], label='Datos reales')
-plt.plot(data[columna_x], Y_pred, color='red', linewidth=2, label='Regresión lineal')
-plt.xlabel(columna_x)
-plt.ylabel(columna_y)
-plt.legend()
-plt.title('Regresión Lineal y Correlación entre X e Y')
-plt.show()
+# Imprimir las predicciones cargadas
+for prediccion in predicciones_cargadas:
+    print(prediccion)
